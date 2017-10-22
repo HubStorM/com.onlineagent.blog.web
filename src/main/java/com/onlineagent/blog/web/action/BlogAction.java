3b900454fcf3c1f87c17a470d2d6f0fc5e9eb5ab
restorm <restorm@live.com>
2017-10-22 22:19:46 +0800

    common commit

--- src/main/java/com/onlineagent/blog/web/action/BlogAction.java
+++ src/main/java/com/onlineagent/blog/web/action/BlogAction.java
@@ -1,10 +1,21 @@
 package com.onlineagent.blog.web.action;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author lijie
  * @date 2017/10/21 17:25
  * @description
  */
+
+@RestController
+@RequestMapping("/blog")
 public class BlogAction {
 
+    @RequestMapping(value = "/insert", method = RequestMethod.GET)
+    public String insert(){
+        return "insert";
+    }
 }
